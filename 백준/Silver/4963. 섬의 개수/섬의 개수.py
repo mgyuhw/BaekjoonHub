1c314e2bbbad5dc20f16ba93b42a58d1fa314933
Hwang Mingyu <58493605+mgyuhw@users.noreply.github.com>
2024-11-13 19:07:17 +0900

    [Silver II] Title: 섬의 개수, Time: 44 ms, Memory: 31452 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -4,26 +4,25 @@ sys.setrecursionlimit(3000)
 dx = (-1, 0, 1, -1, 1, -1, 0, 1)
 dy = (-1, -1, -1, 0, 0, 1, 1, 1)
 g_width = g_height = 0
-g_matrix = g_visited = []
+g_matrix = []
 
 def dfs(y, x):
     for i in range(8):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < g_width and 0 <= ny < g_height and g_matrix[ny][nx] and not g_visited[ny][nx]:
-            g_visited[ny][nx] = True
+        if 0 <= nx < g_width and 0 <= ny < g_height and g_matrix[ny][nx]:
+            g_matrix[ny][nx] = 0
             dfs(ny, nx)
 
 def main():
-    global g_width, g_height, g_visited, g_matrix
+    global g_width, g_height, g_matrix
 
     while True:
         g_width, g_height = map(int, sys.stdin.readline().split())
         if g_width == 0 and g_height == 0: break
 
         g_matrix = [[0] * g_width for _ in range(g_height)]
-        g_visited = [[False] * g_width for _ in range(g_height)]
         count = 0
 
         for i in range(g_height):
@@ -31,8 +30,8 @@ def main():
 
         for i in range(g_height):
             for j in range(g_width):
-                if not g_visited[i][j] and g_matrix[i][j]:
-                    g_visited[i][j] = True
+                if g_matrix[i][j]:
+                    g_matrix[i][j] = 0
                     dfs(i,j)
                     count += 1
 
