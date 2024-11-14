e81286142c53334bbab6494e597df31232ef9d6a
Hwang Mingyu <58493605+mgyuhw@users.noreply.github.com>
2024-11-15 01:47:37 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 636 ms, Memory: 90776 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,37 @@
+import sys
+
+def dfs(vertex, depth):
+    stack = [(vertex, depth)]
+    count = 1
+
+    while stack:
+        vertex, depth = stack.pop()
+        if visited[vertex] == 0:
+            deep[vertex] = depth
+            visited[vertex] = count
+            count += 1
+
+            for v in nodes[vertex]:
+                if visited[v] == 0:
+                    stack.append((v, depth + 1))
+
+if __name__ == "__main__":
+    data = sys.stdin.read().splitlines()
+    node, link, start = map(int, data[0].split())
+    nodes = [[] for _ in range(node + 1)]
+    visited = [0] * (node + 1)
+    deep = [-1] * (node + 1)
+
+    for i in data[1:]:
+        vertex1, vertex2 = map(int, i.split())
+        nodes[vertex1].append(vertex2)
+        nodes[vertex2].append(vertex1)
+
+    for i in range(1, node + 1):
+        nodes[i].sort()
+
+    dfs(start, 0)
+
+    result = [a * b for a, b in zip(visited[1:], deep[1:])]
+
+    sys.stdout.write(str(sum(result)))
