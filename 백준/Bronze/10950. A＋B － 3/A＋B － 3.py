0214c92f99210a15b3eb2c11778e13c1fea0c1c0
Hwang Mingyu <58493605+mgyuhw@users.noreply.github.com>
2025-03-23 02:45:47 +0900

    [Bronze V] Title: A+B - 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,6 +1,9 @@
 import sys
 
 data = list(map(int, sys.stdin.read().split()))
+result = list()
 
 for i in range(1, data[0]*2+1, 2):
-    sys.stdout.write(str(data[i] + data[i+1]) + '\n')
+    result.append(str(data[i] + data[i + 1]))
+
+sys.stdout.write('\n'.join(result))
