f95c07e461efa654ea4dfa02a69a7e4209e3bf44
Hwang Mingyu <58493605+mgyuhw@users.noreply.github.com>
2025-05-18 19:00:40 +0900

    [Bronze III] Title: 최소, 최대, Time: 168 ms, Memory: 4896 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int n, num, max = -1000000, min = 1000000;
-    scanf("%d", &n);
-    for(int a = 0; a<n; a++)
-    {
-        scanf("%d", &num);
-        if(num > max)
-        {
-            max = num;
-        }
-        if(num < min)
-        {
-            min = num;
-        }
+void loot(int);
+
+int max = -1000000, min = 1000000;
+
+int main(void) {
+    int size = 0;
+    scanf("%d", &size);
+
+    int arr[size]; //대신 이 방법은 int arr[size] = {0};처럼 초기화 불가
+
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
+
+    for(int i = 0; i < size; i++) {
+        loot(arr[i]);
+    }
+
     printf("%d %d", min, max);
+
     return 0;
+}
+
+void loot(int num) {
+    if(num > max)
+    {
+        max = num;
+    }
+    if(num < min)
+    {
+        min = num;
+    }
 }
